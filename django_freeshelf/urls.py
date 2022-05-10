28ed3cd4db65da2631424f8d197607befe021369
JDitto95 <powell0295@gmail.com>
2022-05-10 09:27:47 -0400

    added homepage

--- django_freeshelf/urls.py
+++ django_freeshelf/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from books import views as books_views
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('accounts/', include('registration.backends.simple.urls'),)
+    path('accounts/', include('registration.backends.simple.urls'),),
+    path('', books_views.list_books, name='list_books' )
 ]
